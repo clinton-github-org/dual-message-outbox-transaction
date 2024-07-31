ae9764f65bb713902d1649aaebfb5affd02abb48
clinton1719 <clintoncleetus17@gmail.com>
2024-07-31 20:33:02 +0530

    adding aop and exception handling

---
+++ packages/authorization-server/src/main/java/com/clinton/authorization_server/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package com.clinton.authorization_server.exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Clinton Fernandes
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    private static final String serverErrorMessage = "Server error, please try later";
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableExceptions(HttpMessageNotReadableException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", "Invalid JSON values");
+        LOG.error("Invalid JSON values", ex);
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<Map<String, String>> handleSQLExceptions(SQLException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", serverErrorMessage);
+        LOG.error("SQLException" + "\n", ex);
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableExceptions(RuntimeException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", serverErrorMessage);
+        LOG.error("** RuntimeException **" + "\n", ex);
+        return new ResponseEntity<>(errors, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
