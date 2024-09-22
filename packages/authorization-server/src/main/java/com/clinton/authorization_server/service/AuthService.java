ab80f99e2c86a6e00322b2d244bc614892084b0b
clinton1719 <clintoncleetus17@gmail.com>
2024-09-22 11:30:09 +0530

    feat(01): Fixing email

--- packages/authorization-server/src/main/java/com/clinton/authorization_server/service/AuthService.java
+++ packages/authorization-server/src/main/java/com/clinton/authorization_server/service/AuthService.java
@@ -53,7 +53,7 @@ public class AuthService {
                 authorization.setOutbox(outbox);
                 accountRepository.save(account);
                 authorizationRepository.save(authorization);
-                sendNotificationToSES.sendNotification(account.get().getPhoneNumber(), account.getAccountName());
+                sendNotificationToSES.sendNotification(account.getPhoneNumber(), account.getAccountName());
             } else {
                 outbox = new Outbox(Status.DECLINED, authorization);
                 authorization.setOutbox(outbox);
